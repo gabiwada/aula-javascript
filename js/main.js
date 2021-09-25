90c04d34fb93e810f9ce1e5e8643b0d02c68993d
Gabriela Wada <gabiwada13@gmail.com>
2021-09-25 15:14:04 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function botao() {
+  document.getElementById('agradecimento').innerHTML =
+    '<b>Obrigado por clicar</b>'
+  //console.log(document.getElementById("agradecimento"));
+  //alert("Obrigado por clicar");
+}
+
+/*var nome = "Gabriela Wada";
+var idade = 19;
+var idade2= 10;
+var frase= "Japão é um belo país";
+alert(nome + "tem" + idade + "anos");
+alert(idade + idade2)
+*/
